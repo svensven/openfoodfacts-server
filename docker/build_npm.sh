87046da0acdedad3abecbb17afc6fa8a7cae08a4
Sven <svensven@users.noreply.github.com>
2020-10-18 09:42:02 +0100

    make build_npm.sh generate node_modules in the checkout folder

--- docker/build_npm.sh
+++ docker/build_npm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run --rm -it -v node_modules:/mnt/node_modules -v $(cd .. && pwd)/:/mnt -w /mnt node:lts npm install
-docker run --rm -it -v node_modules:/mnt/node_modules -v $(cd .. && pwd)/:/mnt -w /mnt node:lts npm run build
+docker run --rm -it -v $(cd .. && pwd)/node_modules:/mnt/node_modules -v $(cd .. && pwd)/:/mnt -w /mnt node:lts npm install
+docker run --rm -it -v $(cd .. && pwd)/node_modules:/mnt/node_modules -v $(cd .. && pwd)/:/mnt -w /mnt node:lts npm run build
